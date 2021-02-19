e9aaa99a1e3df45fc0300e02e8ca6edbb71cc0f6
yt8492 <tmymyut2000@gmail.com>
2021-02-20 04:37:44 +0900

    fix styles.css

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -9,10 +9,10 @@ repositories {
 
 dependencies {
     api(kotlin("stdlib-js"))
-    api("org.jetbrains:kotlin-react:17.0.1-pre.144-kotlin-1.4.30")
-    api("org.jetbrains:kotlin-react-dom:17.0.1-pre.144-kotlin-1.4.30")
-    api("org.jetbrains:kotlin-css:1.0.0-pre.144-kotlin-1.4.30")
-    api("org.jetbrains:kotlin-styled:5.2.0-pre.144-kotlin-1.4.30")
+    api("org.jetbrains:kotlin-react:17.0.1-pre.148-kotlin-1.4.30")
+    api("org.jetbrains:kotlin-react-dom:17.0.1-pre.148-kotlin-1.4.30")
+    api("org.jetbrains:kotlin-css:1.0.0-pre.148-kotlin-1.4.30")
+    api("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.30")
 }
 
 kotlin {
